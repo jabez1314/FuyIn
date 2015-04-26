217149ef832ebaff41c718010480e112870b73f1
jabez <liyan@quaner.com.cn>
2015-04-26 08:48:11 +0800

    init

---
+++ FuYin/FuYin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FuYin
+//
+//  Created by Jabez on 15/4/26.
+//  Copyright (c) 2015年 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FuYin/FuYin/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FuYin
+//
+//  Created by Jabez on 15/4/26.
+//  Copyright (c) 2015年 John. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
